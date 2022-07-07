13a5065734133a2492cbc6e08f2b7cbcdf8b84fa
FathimaZahrah <zahrahshafik13@gmail.com>
2022-07-07 21:03:55 +0530

    add game added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddgameComponent } from './addgame/addgame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddgameComponent
   ],
   imports: [
     BrowserModule,
